0578ca7e80e9fd4e34634e022f7717304fe6e28e
Riya Tripathi <86204416+riyatripathi@users.noreply.github.com>
2023-06-18 17:48:38 +0530

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -6,7 +6,6 @@ let input = document.getElementsByClassName('input')[0];
 
 let button = document.getElementsByClassName('find');
 let meaning = document.getElementsByClassName('meaning')[0];
-let origin = document.getElementsByClassName('origin')[0];
 let synonym = document.getElementsByClassName('synonym')[0];
 let antonym = document.getElementsByClassName('antonym')[0];
 
@@ -19,10 +18,9 @@ button[0].addEventListener("click", function(){
 	ajax.onload = function(){
 		let ans = JSON.parse(ajax.responseText);
 		console.log(ans);
-		antonym.innerHTML = `<span class="text-danger">Antonym:</span> ${ans[0].meanings[0].definitions[0].antonyms[0]}`;
-		synonym.innerHTML = `<span class="text-danger">Synonym:</span> ${ans[0].meanings[0].definitions[0].synonyms[0]}`;
+		antonym.innerHTML = `<span class="text-danger">Antonym:</span> ${ans[0].meanings[0].antonyms[0]}`;
+		synonym.innerHTML = `<span class="text-danger">Synonym:</span> ${ans[0].meanings[0].synonyms[0]}`;
 		meaning.innerHTML = `<span class="text-danger">Meaning:</span> ${ans[0].meanings[0].definitions[0].definition}`;
-		origin.innerHTML = `<span class="text-danger">Origin:</span> ${ans[0].origin}`;
 	}
 })
 
